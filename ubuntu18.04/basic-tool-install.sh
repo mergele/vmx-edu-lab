50417cc96a7ab101e0c19efe32dde728f22836b2
Felix Mergele <felix@mergele.org>
2018-11-26 21:48:58 +0100

    changes to install the packages with the "-y" option

--- ubuntu18.04/basic-tool-install.sh
+++ ubuntu18.04/basic-tool-install.sh
@@ -4,6 +4,6 @@
 # 
 # run as root...
 apt update && apt upgrade -y
-apt install open-vm-tools open-vm-tools-desktop
-apt install git tmux vim mc htop 
-apt autoremove
+apt install open-vm-tools open-vm-tools-desktop -y
+apt install git tmux vim mc htop -y
+apt autoremove -y
