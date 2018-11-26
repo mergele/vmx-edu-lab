e6aeff6f856d05e5aeda0d42ada36666f9389e8c
Felix Mergele <felix@mergele.org>
2018-11-26 21:35:36 +0100

    Create grpc-setup.sh

---
+++ ubuntu18.04/grpc-setup.sh
@@ -0,0 +1,6 @@
+# install grpc for python3 following the guide on https://grpc.io/docs/quickstart/python.html
+
+#!/bin/bash
+apt install python3-pip
+pip3 install grpcio
+pip3 install grpcio-tools googleapis-common-protos
